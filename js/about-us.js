a44d8271ddecd6f6fe61a8dfdeecc92cc3122bd7
Yash Pathak <yashpathak339@gmail.com>
2019-12-13 22:29:29 -0500

    [IMP][ADD]: updated the about us and contest page

--- js/about-us.js
+++ js/about-us.js
@@ -1,13 +1,15 @@
+// changing the background and font color on hover of about us
 $('#image-text').hover(
     function(){$(this).css({
-        'background': '#FF8C94',
-        'color': '#A8E6CE'
+        'background': '#A8E6CE',
+        'color': '#FF8C94'
     });},
     function(){$(this).css({
-        'background': '#A8E6CE',
+        'background': '#FF8C94',
         'color': 'white'
     });}
 )
+//changing the font and color on hover
 $('#history').hover(
     function(){$(this).css({
         'background': '#A8E6CE',
@@ -18,3 +20,26 @@ $('#history').hover(
         'color': 'white'
     });}
 )
+
+//on hover of image showing the name
+function showDetails(divid){
+    if (divid === "div1"){
+        document.getElementById(divid).innerHTML = "Yash(Owner)";
+    }
+    else if (divid === "div2"){
+        document.getElementById(divid).innerHTML = "Krishna (Lead Baker)";
+    }
+    else if (divid === "div3"){
+        document.getElementById(divid).innerHTML = "Marl (Associate Baker)";
+    }
+    else if (divid === "div4"){
+        document.getElementById(divid).innerHTML = "Sukhdeep (Manager)";
+    }
+    else {
+        document.getElementById(divid).innerHTML = "Amit(Supervisor)";
+    }
+}
+//hide the details of image on mouse out
+function hideDetails(divid){
+    document.getElementById(divid).innerHTML = "";
+}
--- js/contest.js
+++ js/contest.js
@@ -1,6 +1,3 @@
-//to do 
-//add comments
-
 // defining the win condition
 var winMoves = [
     [1,2,3],
@@ -13,10 +10,14 @@ var winMoves = [
     [3,5,7]
 ];
 
+// defining the boardmoves
 var boardMoves = [0,0,0,0,0,0,0,0,0,0];
+//initalizing the turn
 var turns = 0;
 result_element = document.getElementById("game_result")
 
+
+//this method will check if the match is over or not
 function checkWin()
 {
     var win = false;
@@ -24,95 +25,118 @@ function checkWin()
     {
         if (boardMoves[winMoves[i][0]] == 'o' && boardMoves[winMoves[i][1]] == 'o' && boardMoves[winMoves[i][2]] == 'o')
         {
-            result_element.innerHTML = "Computer, wins!\nClick on reset to play again!"
+            result_element.innerHTML = "Computer wins!😱 Click on reset to play again!"
+            $("#" + winMoves[i][0]).addClass("turn lost");
+            $("#" + winMoves[i][1]).addClass("turn lost");
+            $("#" + winMoves[i][2]).addClass("turn lost");
+            $('[class^="box-"]').css('perspective', 'none');
             win = true;
         }
         else if (boardMoves[winMoves[i][0]] == 'x' && boardMoves[winMoves[i][1]] == 'x' && boardMoves[winMoves[i][2]] == 'x')
         {
-            result_element.innerHTML = "You win!! Congratulations!"
+            result_element.innerHTML = "You win!! Congratulations!🤗"
+            $("#" + winMoves[i][0]).addClass("turn win");
+            $("#" + winMoves[i][1]).addClass("turn win");
+            $("#" + winMoves[i][2]).addClass("turn win");
+            $('[class^="box-"]').css('perspective', 'none');
             win = true;
         }
     }
     if (win){
+        // if the game is over, then the below code will remove the onclick
         result_element.style.display = "block";
         for (i=1; i<10; i++){
             div = document.getElementById(i);
-            div.removeEventListener("onclick", checkClick())
+            div.removeAttribute('onclick');
         }
     }
     return win;
 }
 
+
+//this method returns the id of the element where the computer should place the next element
+//here I am just checking if there is possibility of winning or losing, if there is then it will return the element id
 function getBox(condition)
 {
     for (i = 0; i < winMoves.length; i++)
     {
         if (boardMoves[winMoves[i][0]] == condition && boardMoves[winMoves[i][1]] == condition && boardMoves[winMoves[i][2]] == '0')
         {
-            debugger
             return winMoves[i][2];
         }
         else if (boardMoves[winMoves[i][0]] == condition && boardMoves[winMoves[i][2]] == condition && boardMoves[winMoves[i][1]] == '0')
         {
-            debugger
             return winMoves[i][1];
         }
         else if (boardMoves[winMoves[i][1]] == condition && boardMoves[winMoves[i][2]] == condition && boardMoves[winMoves[i][0]] == '0')
         {
-            debugger
             return winMoves[i][0];
         }
     }
     return false;
 }
 
+
+//checking the user click here
 function checkClick(boardPosition)
 {
 
     if (boardMoves[boardPosition] === 0)
     {
         var position = document.getElementById(boardPosition);
+        
         position.innerHTML = 'x';
         boardMoves[boardPosition] = 'x';
         turns ++;
+        //checking if the game is over
         var win = checkWin();
+        //calling the computer turn
         if (!win){
             smartComputer();
         }
     }
 }
 
+
+//computer turn
 function smartComputer()
 {
+    //if win is 9 means it's draw
     if (turns >= 9) 
        {
-        result_element.innerHTML = "Draw! What a smart game!\nClick on reset to play again!"
+        result_element.innerHTML = "Draw! What a smart game!😎\nClick on reset to play again!"
         result_element.style.display = "block";
         tunrs ++;
+        //game is over removing onclick
         for (i=1; i<10; i++){
             div = document.getElementById(i);
-            div.removeEventListener("onclick", checkClick())
+            div.removeAttribute('onclick');
         }
         return false;
        }
+    //to prevent the case of losing, if the user doesn't place anything in exact middle, this will put the "O" in middle
     if (boardMoves[5] === 0)
     {
         computerMove = 5;
     }
+    //checking if we are winning then play offensive
     else if (getBox('o'))
     {
         computerMove = getBox('o')
     }
+    //checking if the user is winning then play defense
     else if (getBox('x'))
     {
         computerMove = getBox('x')
     }
+    //when there is no winning or losing possibility this scenario will be return
     else {
         do 
         {
-            debugger
+            // taking anything between 1 to 9
             computerMove = Math.ceil(Math.random() * 9);
         }
+        // checking if the random number is not already selected
         while(boardMoves[computerMove] != 0);
     }
     var position = document.getElementById(computerMove);
@@ -123,12 +147,22 @@ function smartComputer()
 }
 
 document.getElementById("reset-button").onclick = function (){
-    boardMoves = [0,0,0,0,0,0,0,0,0,0];
-    turns = 0;
-    for (i=1; i<10; i++){
-        position = document.getElementById(i);
-        position.innerHTML = '';
+    // reloading the page to reset everything
+    location.reload();
+    // boardMoves = [0,0,0,0,0,0,0,0,0,0];
+    // turns = 0;
+    // for (i=1; i<10; i++){
+    //     position = document.getElementById(i);
+    //     position.innerHTML = '';
+    //     document.getElementById(i).setAttribute("onclick", "1");
+    // }
+    // result_element.innerHTML = ""
+    // result_element.style.display = "none";
     }
-    result_element.innerHTML = ""
-    result_element.style.display = "none";
-    }
+
+
+//this will give the intro element when the page is loaded
+$("#intro").animate({ top: "200px", opacity: "1" }, 1100);
+$( "#intro_button" ).click(function() {
+    $("#intro").fadeOut(1000);
+});
